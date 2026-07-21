b446bbdaed0d5d83134429d6839470794bac956f
JensDDNN <info@domela.eu>
2026-07-21 13:45:56 +0200

    Change Home component to RootLayout
    Refactor Home component to RootLayout for layout structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-export default function Home() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <main style={{ padding: "4rem" }}>
-      <h1>JENS DOMELA NIEUWENHUIS</h1>
-
-      <h2>Fine Art Photography</h2>
-
-      <p>
-        Exploring silence, presence and urban space.
-      </p>
-    </main>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
