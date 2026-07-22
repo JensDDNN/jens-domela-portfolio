6f91a4417e29e6d23d32cecf975d60c3e54c4232
JensDDNN <info@domela.eu>
2026-07-22 12:47:37 +0200

    Refactor page layout and update image source
    Updated image source and modified heading styles.

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,7 @@ export default function Home() {
         padding: "40px",
       }}
     >
-      <Image
-        src="/DSC_17401.jpg"
+      "
         alt="Artificial Trees"
         width={1200}
         height={800}
@@ -23,7 +22,19 @@ export default function Home() {
 
       <h1
         style={{
-          fontSize: "ace.
+          fontSize: "5rem",
+          lineHeight: 1,
+        }}
+      >
+        JENS DOMELA
+        <br />
+        NIEUWENHUIS
+      </h1>
+
+      <h2>Fine Art Photography</h2>
+
+      <p>
+        Exploring silence, presence and urban space.
       </p>
 
       <hr style={{ margin: "4rem 0" }} />
