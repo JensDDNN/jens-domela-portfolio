8fcf0ea4bf9c2a27ee6546b41563e0b9e01c4e39
JensDDNN <info@domela.eu>
2026-07-24 12:06:00 +0200

    Create WinterSilence component with image and text
    Added a new WinterSilence component with an image and description.

--- public/images/winter-silence/page.tsx
+++ public/images/winter-silence/page.tsx
@@ -1,1 +1,17 @@
+import Image from "next/image";
 
+export default function WinterSilence() {
+  return (
+    <main style={{ padding: "4rem" }}>
+      <h1>Winter Silence</h1>
+
+      <p>
+        A photographic exploration of atmosphere,
+        absence and stillness.
+      </p>
+
+      <Image
+        src="/images/winter-silence/DSC_1344.jpg"
+        alt="Winter Canal"
+        width={1200}
+        height={
