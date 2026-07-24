f8df29119198440ed9c42877e66bbc5cc6d8372d
JensDDNN <info@domela.eu>
2026-07-24 12:09:53 +0200

    Remove unused Image import in WinterSilence
    Removed unused Image import from WinterSilence component.

--- public/images/winter-silence/page.tsx
+++ public/images/winter-silence/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function WinterSilence() {
   return (
     <main style={{ padding: "4rem" }}>
@@ -9,9 +7,6 @@ export default function WinterSilence() {
         A photographic exploration of atmosphere,
         absence and stillness.
       </p>
-
-      <Image
-        src="/images/winter-silence/DSC_1344.jpg"
-        alt="Winter Canal"
-        width={1200}
-        height={
+    </main>
+  );
+}
